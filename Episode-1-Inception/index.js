8fd1d68856f9186af3868c89113f7bac13de465f
Sachin-Dabgar <bosscoder1325@gmail.com>
2023-03-27 06:10:30 +0530

    added gitignore to the inception

---
+++ Episode-1-Inception/index.js
@@ -0,0 +1,24 @@
+/**
+ * hot module reload
+ * file watcher algorithm - c++
+ * bundling, minification
+ * cleaning the code
+ * dev and production build
+ * super fast build algorithm
+ * caching while development
+ * compression
+ * compatible with older version of browsers
+ * to enable https on dev machine... hehe [npx parcel index.html --https]
+ * consistent hashing algorithm
+ * 
+ * npx parcel index.html
+ * npx parecel build index.html
+ */
+
+
+import React from "react"
+import ReactDOM from "react-dom/client"
+
+const heading = React.createElement("h1", {}, "Namaste Everyone")
+const root = ReactDOM.createRoot(document.getElementById("root"))
+root.render(heading)
